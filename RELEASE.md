a84c873d4aee023632758c12d641331918e91c5e
goanpeca <goanpeca@gmail.com>
2020-06-16 11:02:29 -0500

    Release version

--- RELEASE.md
+++ RELEASE.md
@@ -1,1 +1,9 @@
 # Release
+
+```bash
+git tag -a vX.X.X -m 'Release version vX.X.X'
+```
+
+```bash
+python setup.py sdist bdist_wheel
+```
--- zenhub/__init__.py
+++ zenhub/__init__.py
@@ -10,7 +10,7 @@
 from .core import ZenhubError, APILimitError, NotFoundError, InvalidTokenError, Zenhub
 
 
-__version__ = "0.1.0.dev0"
+__version__ = "0.1.0"
 
 
 def _to_version_info(version):
