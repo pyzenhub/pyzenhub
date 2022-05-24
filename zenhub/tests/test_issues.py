5d8680ef5cc7ac8948366f880fbc3cd79b033a90
Gonzalo Pena-Castellanos <goanpeca@gmail.com>
2022-05-24 15:27:05 -0500

    Add initial tests

--- zenhub/tests/test_issues.py
+++ zenhub/tests/test_issues.py
@@ -1,17 +1,19 @@
 """Test ZenHub issues API."""
-from zenhub import Zenhub, ZenhubError
+import os
 
 import pytest
 
+from zenhub import Zenhub, ZenhubError
 
-TOKEN = ''
+
+TOKEN = os.environ.get("ZENHUB_TEST_TOKEN")
 REPO_ID = 262640661
 
 
 def test_get_issue_data():
     zh = Zenhub(TOKEN)
     data = zh.get_issue_data(REPO_ID, 1)
-    assert list(data.keys()) == ['plus_ones', 'is_epic', 'pipelines', 'pipeline']
+    assert list(data.keys()) == ["plus_ones", "is_epic", "pipelines", "pipeline"]
 
 
 def test_get_issue_data_invalid():
